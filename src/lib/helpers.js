018aafbc135ec62b3f118cfbd589095b769dff7d
Bryce Flory <iam@bryceflory.com>
2022-06-30 18:23:46 +0200

    header, hero, featured, post entry, post meta, sidebar, recent post widgets created

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,15 +1,15 @@
-import { parseISO, format } from 'date-fns';
-import { client } from '../lib/sanityClient.js';
-import imageUrlBuilder from '@sanity/image-url';
+import { parseISO, format } from 'date-fns'
+import { client } from '../lib/sanityClient.js'
+import imageUrlBuilder from '@sanity/image-url'
 
-const builder = imageUrlBuilder(client);
+const builder = imageUrlBuilder(client)
 
 export function formatBlogPostDate(date) {
-  const dateString = parseISO(date, 'YYYY/MM/Do');
-  const formattedDateString = format(dateString, 'MMMM do, yyyy');
-  return `${formattedDateString}`;
+  const dateString = parseISO(date, 'YYYY/MM/Do')
+  const formattedDateString = format(dateString, 'MMMM do, yyyy')
+  return `${formattedDateString}`
 }
 
 export function getSanityImageUrl(source) {
-  return builder.image(source);
+  return builder.image(source)
 }
--- src/lib/queries.js
+++ src/lib/queries.js
@@ -1,1 +1,4 @@
-export const allPosts = `*[_type == 'article'] | order(publishDate desc)`
+export const allPosts = `*[_type == 'article']{..., categories[] ->, country ->} | order(publishDate desc)`
+export const RecentPosts = `*[_type == "article"] | order(_createdAt desc)[0...5]`
+
+export const siteSettings = `*[_type == 'siteSettings']`
