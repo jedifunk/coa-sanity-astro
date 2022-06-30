78f2d14522fcdff326efda45e01abb9bf6bc6b44
Bryce Flory <iam@bryceflory.com>
2022-06-30 20:51:43 +0200

    sidebar widgets added

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -1,4 +1,6 @@
 export const allPosts = `*[_type == 'article']{..., categories[] ->, country ->} | order(publishDate desc)`
-export const RecentPosts = `*[_type == "article"] | order(_createdAt desc)[0...5]`
+export const recentPosts = `*[_type == "article"] | order(_createdAt desc)[0...5] {title, slug}`
+export const countryList = `*[_type == "country"] | order(name asc) {name, menu, slug}`
+export const categoryList = `*[_type == "category"] | order(title asc) {title, slug}`
 
 export const siteSettings = `*[_type == 'siteSettings']`
