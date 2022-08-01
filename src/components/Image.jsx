03028570230fb65ce9c2d18d1d58bc9be45fe837
Bryce Flory <iam@bryceflory.com>
2022-08-01 19:11:27 +0200

    breaks, embed, image, quotation migrated to astro

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -4,12 +4,12 @@ const Image = ({value}) => {
 
   return (
     <>
-    <figure className={`img-full`}>
+    {/* <figure className={`img-full`}>
       <button >
         <img src={getSanityImageUrl(value.asset)} />
         {value.caption && <figcaption>{value.caption}</figcaption>}
       </button>
-    </figure>
+    </figure> */}
     </>
   )
 }
