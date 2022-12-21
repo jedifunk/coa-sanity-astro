db46b1fcd6e633cd0a69dbf39fc8a95cfaea808b
Bryce Flory <iam@bryceflory.com>
2022-12-21 12:46:03 +0200

    exif undefined fix

--- src/components/GalleryBlock.jsx
+++ src/components/GalleryBlock.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import Lightbox from "yet-another-react-lightbox"
 import { sSpeed, getSanityImageUrl } from "../lib/helpers"
-// import {BiExpandAlt} from 'react-icons/bi'
+import {BiExpandAlt} from 'react-icons/bi'
 import 'yet-another-react-lightbox/styles.css'
 import Captions from "yet-another-react-lightbox/plugins/captions"
 import "yet-another-react-lightbox/plugins/captions.css"
@@ -13,9 +13,9 @@ export default function GalleryBlock({ value }) {
   const slides = value.images.map((img) => ({
     src: getSanityImageUrl(img).auto('format').quality(100).url(),
     title: img.caption,
-    description: `${img.asset.metadata.exif ? `Camera: ${img.asset.metadata.exif.LensModel}, Aperture: f/${img.asset.metadata.exif.FNumber}, ISO: ${img.asset.metadata.exif.ISO}, Shutter Speed: ${sSpeed(img.asset.metadata.exif.ExposureTime)}` : ``}`
+    description: `${img?.asset?.metadata?.exif ? `Camera: ${img?.asset?.metadata?.exif?.LensModel}, ISO: ${img?.asset?.metadata?.exif?.ISO}, Shutter Speed: ${sSpeed(img?.asset?.metadata?.exif?.ExposureTime)}` : ``}`
   }))
-console.log(slides)
+
   // Set State for Lightbox
   const [photoIndex, setPhotoIndex] = useState(0)
   const [isOpen, setIsOpen] = useState(false)
@@ -23,38 +23,36 @@ console.log(slides)
   return (
     <>
     <div className={`blocks-gallery-grid columns-${cols}`}>
-        {value.images.map((img, index) => {
-          const image = getSanityImageUrl(img).url()
+      {value.images.map((img, index) => {
+        const image = getSanityImageUrl(img).url()
 
-          // setup onclick function to handle state change
-          function updateOnClick() {
-            setPhotoIndex(index)
-            setIsOpen(true)
-          }
-          
-          return (
-            <figure key={index} className="blocks-gallery-item">
-              <button onClick={updateOnClick}>
-                {/* <BiExpandAlt className='enlarge'/> */}
-                <img src={getSanityImageUrl(img).width(750).auto('format').url()} loading="lazy" alt={img.alt}/>
-                {img.caption && <figcaption>{img.caption}</figcaption>}
-              </button>
-            </figure>
-          )
-        })}
-      </div>
+        // setup onclick function to handle state change
+        function updateOnClick() {
+          setPhotoIndex(index)
+          setIsOpen(true)
+        }
+        
+        return (
+          <figure key={index} className="blocks-gallery-item">
+            <button onClick={updateOnClick}>
+              <BiExpandAlt className='enlarge'/>
+              <img src={getSanityImageUrl(img).width(750).auto('format').url()} loading="lazy" alt={img.alt}/>
+              {img.caption && <figcaption>{img.caption}</figcaption>}
+            </button>
+          </figure>
+        )
+      })}
+    </div>
 
-      <Lightbox
-        open={isOpen}
-        close={() => setIsOpen(false)}
-        slides={slides}
-        index={photoIndex}
-        plugins={[Captions]}
-        animation={{swipe: 0}}
-        controller={{closeOnBackdropClick: true}}
-      />
-
+    <Lightbox
+      open={isOpen}
+      close={() => setIsOpen(false)}
+      slides={slides}
+      index={photoIndex}
+      plugins={[Captions]}
+      animation={{swipe: 0}}
+      controller={{closeOnBackdropClick: true}}
+    />
     </>
   )
-
 }
