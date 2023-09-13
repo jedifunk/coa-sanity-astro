f314ec17acb2fad8150c8834663922a795092b9a
Bryce Flory <iam@bryceflory.com>
2023-09-13 13:41:19 +0300

    fixes to images to include astro-sanity-picture

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config'
+//import {loadEnv} from 'vite'
 import react from "@astrojs/react"
 import sanity from '@sanity/astro'
 import partytown from "@astrojs/partytown";
+//const {PUBLIC_ID} = loadEnv(process.env.PUBLIC_SANITY_PROJECT_ID, process.cwd(), "")
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,10 +15,15 @@ export default defineConfig({
     // }), 
     sanity({
       projectId: "8icb2evz",
+      //projectId: PUBLIC_ID,
       dataset: 'production',
       apiVersion: '2023-08-09',
       useCdn: false,
     }),
     react(),
-  ]
+  ],
+  image: {
+    domains: ['https://graph.instagram.com/'],
+    remotePatterns: [{protocol: 'https'}],
+  }
 })
