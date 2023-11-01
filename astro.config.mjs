a23f1917e2087a7faab1f0338d760c2c515a2d82
Bryce Flory <iam@bryceflory.com>
2023-11-01 11:47:22 +0000

    @sanity/astro and pagination updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from 'astro/config'
-//import {loadEnv} from 'vite'
 import react from "@astrojs/react"
 import sanity from '@sanity/astro'
 import partytown from "@astrojs/partytown";
-//const {PUBLIC_ID} = loadEnv(process.env.PUBLIC_SANITY_PROJECT_ID, process.cwd(), "")
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,7 +13,6 @@ export default defineConfig({
     // }), 
     sanity({
       projectId: "8icb2evz",
-      //projectId: PUBLIC_ID,
       dataset: 'production',
       apiVersion: '2023-08-09',
       useCdn: false,
