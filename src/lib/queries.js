8038066c60b5e82f9adb426d301784c61201c2b1
Bryce Flory <iam@bryceflory.com>
2024-07-16 13:13:17 +0300

    update for array of countries or cities per article

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -3,13 +3,13 @@ export const siteSettings = `*[_type == 'siteSettings']`
 export const siteNav = `*[_type == 'siteSettings'][0]{siteNav{menuItems[]{name, page->{slug}}}}`
 
 // Page & Posts
-export const allPosts = `*[_type == 'article']{..., categories[] ->, country ->, content[]{..., markDefs[]{..., _type=='internalLink'=>{..., "slug": @.reference->slug}}, asset->, images[]{..., asset->}}} | order(publishDate desc)`
+export const allPosts = `*[_type == 'article']{..., categories[] ->, countries[] ->, content[]{..., markDefs[]{..., _type=='internalLink'=>{..., "slug": @.reference->slug}}, asset->, images[]{..., asset->}}} | order(publishDate desc)`
 export const allPagesQuery = `*[_type == 'page']{..., content[]{..., images[]{..., asset->}}}`
 export const categoryList = `*[_type == "category"] | order(title asc) {title, slug}`
 
 // Sidebar
-export const allCountryPostsQuery = `*[_type == 'country']{..., 'relatedArticles': *[_type == 'article' && references(^._id)]{..., country ->, categories[] ->} | order(publishDate desc)}`
-export const allCategoryPostsQuery = `*[_type == 'category']{..., 'relatedArticles': *[_type == 'article' && references(^._id)]{..., country ->, categories[] ->} | order(publishDate desc)}`
+export const allCountryPostsQuery = `*[_type == 'country']{..., 'relatedArticles': *[_type == 'article' && references(^._id)]{..., countries[] ->, categories[] ->} | order(publishDate desc)}`
+export const allCategoryPostsQuery = `*[_type == 'category']{..., 'relatedArticles': *[_type == 'article' && references(^._id)]{..., countries[] ->, categories[] ->} | order(publishDate desc)}`
 export const recentPosts = `*[_type == "article"] | order(_createdAt desc)[0...5] {title, slug}`
 
 // Map related
