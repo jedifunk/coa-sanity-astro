c24187dabdbf120f9b5bd964800e347ecc297486
Bryce Flory <iam@bryceflory.com>
2025-09-14 12:11:02 +0900

    more attempst to fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,24 @@
 import { defineConfig } from 'astro/config'
-import react from "@astrojs/react"
-import sanity from '@sanity/astro'
 import icon from 'astro-icon'
+import sanity from '@sanity/astro'
+import react from '@astrojs/react'
 //import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    // partytown({
-    //   config: {
-    //     forward: ["dataLayer.push"],
-    //   },
-    // }), 
-    sanity({
-      projectId: "8icb2evz",
-      dataset: 'production',
-      apiVersion: '2023-08-09',
-      useCdn: false,
-    }),
-    react(),
-    icon(),
-  ],
+  integrations: [// partytown({
+  //   config: {
+  //     forward: ["dataLayer.push"],
+  //   },
+  // }), 
+  sanity({
+    projectId: "8icb2evz",
+    dataset: 'production',
+    apiVersion: '2023-08-09',
+    useCdn: false,
+  }),
+  react(),
+  icon()],
   image: {
     domains: ['https://graph.instagram.com/'],
     remotePatterns: [{protocol: 'https'}],
